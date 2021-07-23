f9755750c9fce50761bbd8c82d1ed307e3696500
vgaranin <vasilii_garanin@laptevfamily.com>
2021-07-23 12:49:01 -0500

    all tasks done

--- index.js
+++ index.js
@@ -233,15 +233,32 @@ console.log(cuboid.surfaceArea()); // 130
 
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
-class CuboidMakerTwo
+class CuboidMakerTwo 
 {
+    constructor(length, width, height)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
 
+    volume()
+    {
+        return this.length * this.width * this.height;
+    }
+
+    surfaceArea()
+    {
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
 }
+
+const cuboidTwo = new CuboidMakerTwo(4, 5, 5);
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
 
 
